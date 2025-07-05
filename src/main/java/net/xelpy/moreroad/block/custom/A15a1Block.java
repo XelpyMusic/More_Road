426a5736c75fc1926c7d455e7d37abda929ffbad
Xelpy <lucas.uzerche19@gmail.com>
2025-07-05 22:08:11 +0200

    Ajout de la moitié des panneaux.

---
+++ src/main/java/net/xelpy/moreroad/block/custom/A15a1Block.java
@@ -0,0 +1,51 @@
+package net.xelpy.moreroad.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class A15a1Block extends HorizontalDirectionalBlock {
+    public static final MapCodec<A15a1Block> CODEC = simpleCodec(A15a1Block::new);
+    private static final VoxelShape SHAPE_NORTH = Block.box(0, 0, 7, 16, 14, 10);
+    private static final VoxelShape SHAPE_SOUTH = Block.box(0, 0, 6, 16, 14, 9);
+    private static final VoxelShape SHAPE_EAST = Block.box(6, 0, 0, 9, 14, 16);
+    private static final VoxelShape SHAPE_WEST = Block.box(7, 0, 0, 10, 14, 16);
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    public A15a1Block(Properties pProperties) {
+        super(pProperties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return switch (pState.getValue(FACING)) {
+            case SOUTH -> SHAPE_SOUTH;
+            case EAST -> SHAPE_EAST;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+}
